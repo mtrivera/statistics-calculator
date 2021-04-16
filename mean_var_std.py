e185b2ae366496ff3a12a67ec03404ca0b36de13
Miguel T Rivera <mrivera108@outlook.com>
2021-04-15 20:31:47 -0700

    Move logic into Calculate class

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,43 +1,9 @@
-import numpy as np
+import calculate as calc
 
 def calculate(list):
     if len(list) != 9:
         raise ValueError('List must contain nine numbers.')
-    
-    calculations = {}
-    matrix = np.reshape(np.array(list), (3, 3))
-    
-    calculations['mean'] = [
-        np.mean(matrix, axis=0).tolist(),
-        np.mean(matrix, axis=1).tolist(),
-        np.mean(list).tolist(),
-    ]
 
-    calculations['variance'] = [
-        np.var(matrix, axis=0).tolist(),
-        np.var(matrix, axis=1).tolist(),
-        np.var(list).tolist(),
-    ]
+    matrix = calc.Calculate(list)
 
-    calculations['standard deviation'] = [
-        np.std(matrix, axis=0).tolist(),
-        np.std(matrix, axis=1).tolist(),
-        np.std(list).tolist(),
-    ]
-    calculations['max'] = [
-        np.amax(matrix, axis=0).tolist(),
-        np.amax(matrix, axis=1).tolist(),
-        np.amax(list).tolist(),
-    ]
-    calculations['min'] = [
-        np.amin(matrix, axis=0).tolist(),
-        np.amin(matrix, axis=1).tolist(),
-        np.amin(list).tolist(),
-    ]
-    calculations['sum'] = [
-        np.sum(matrix, axis=0).tolist(),
-        np.sum(matrix, axis=1).tolist(),
-        np.sum(list).tolist(),
-    ]
-
-    return calculations
+    return matrix.get_calculations()
